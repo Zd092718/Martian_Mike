30ffb7021c51f76c14724629ef08e2c35bf2e13c
Zack Dowd <zdowd2796@gmail.com>
2023-09-28 13:02:25 -0600

    added win screen

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @export var next_level: PackedScene = null
 @export var level_time = 5
+@export var is_final_level = false
 
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var deathzone = $Deathzone
 @onready var hud = $UILayer/HUD
+@onready var ui_layer = $UILayer
 
 var player = null
 
@@ -67,9 +69,12 @@ func reset_player():
 
 func _on_exit_body_entered(body):
 	if body is Player:
-		exit.animate()
-		player.active = false
-		win = true
-		await get_tree().create_timer(1.5).timeout
-		if next_level != null:
-			get_tree().change_scene_to_packed(next_level)
+		if is_final_level || (next_level != null):
+			exit.animate()
+			player.active = false
+			win = true
+			await get_tree().create_timer(1.5).timeout
+			if is_final_level:
+				ui_layer.show_win_screen(true)
+			else:
+				get_tree().change_scene_to_packed(next_level)
